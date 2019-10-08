e44f07b0053f84c99b37a4fdd04671f025fd593a
canra <canraaripin7@gmail.com>
2019-10-08 16:36:52 +0700

    comit map

--- app/src/main/java/com/goteknisi/ui/dashboarcus/DashboarCusFragment.kt
+++ app/src/main/java/com/goteknisi/ui/dashboarcus/DashboarCusFragment.kt
@@ -2,11 +2,11 @@ package com.goteknisi.ui.dashboarcus
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.goteknisi.R
+import kotlinx.android.synthetic.main.dashboar_cus_fragment.*
 
 class DashboarCusFragment : Fragment() {
 
@@ -15,6 +15,28 @@ class DashboarCusFragment : Fragment() {
     }
 
     private lateinit var viewModel: DashboarCusViewModel
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_Home -> {
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.constMainAct,DashboardListFragment.newInstance())
+                    .commitNow()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_nearby -> {
+                childFragmentManager.beginTransaction()
+                    .replace(R.id.constMainAct,DashboardNearby.newInstance())
+                    .commitNow()
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_history -> {
+
+            }
+        }
+        false
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,9 +48,10 @@ class DashboarCusFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(DashboarCusViewModel::class.java)
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
         if (savedInstanceState == null) {
             childFragmentManager.beginTransaction()
-                .replace(R.id.constMainAct,DashboardNearby.newInstance())
+                .replace(R.id.constMainAct,DashboardListFragment.newInstance())
                 .commitNow()
         }
     }
